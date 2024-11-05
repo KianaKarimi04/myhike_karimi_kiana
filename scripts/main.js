668b6212c510315311407f1a9ee12938b18e2634
kiana <kianakarimi8383@gmail.com>
2024-11-05 14:40:10 -0800

    done demo10

--- scripts/main.js
+++ scripts/main.js
@@ -104,6 +104,7 @@ function displayCardsDynamically(collection) {
                 var details = doc.data().details;  // get value of the "details" key
 								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 var hikeLength = doc.data().length; //gets the length field
+                var docId = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
@@ -111,7 +112,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-
+                newcard.querySelector("a").href = "eachhike.html?docID" + docId;
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
